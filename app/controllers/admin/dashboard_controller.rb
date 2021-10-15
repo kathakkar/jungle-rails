38f350667a0bf85e65998ddb01dd174556039976
Khushbu <khushbu.thakkar@outlook.com>
2021-10-14 21:02:44 -0700

    Add category listing, Add new features on admin side

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  # http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], only: [:show]
+
   def show
     @products_count = Product.all.count
     @categories_count = Category.all.count

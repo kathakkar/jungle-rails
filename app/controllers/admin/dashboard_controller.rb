e7949504ce86be5e16068547f46de68b9685c169
Khushbu <khushbu.thakkar@outlook.com>
2021-10-18 16:46:09 -0700

    Add user registration and login

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  # http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], only: [:show]
-
+  
   def show
     @products_count = Product.all.count
     @categories_count = Category.all.count
